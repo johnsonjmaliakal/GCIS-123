e972cfffe38b138d171a15400586991e7d22ea12
johnsonjmaliakal <johnsonjmaliakal@gmail.com>
2021-11-13 12:13:16 +0400

    Created function blocks for each question

---
+++ Assignment-2/Assignment 2.py
@@ -0,0 +1,24 @@
+def Q1a():
+    pass
+def Q1b():
+    pass
+def Q1c():
+    pass
+def Q1d():
+    pass
+
+def Q2a():
+    pass
+def Q2b():
+    pass
+def Q2c():
+    pass
+def Q2d():
+    pass
+
+def Q3a():
+    pass
+
+def main():
+    pass
+main()
