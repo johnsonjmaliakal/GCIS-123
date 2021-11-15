ccb02bcc9eede9edf5b17c0472a663bfd9052c67
johnsonjmaliakal <johnsonjmaliakal@gmail.com>
2021-11-15 21:45:59 +0400

    updated sorts.py

--- sorts.py
+++ sorts.py
@@ -1,6 +1,4 @@
 import array
-def swap(array, a, b):
-    array[b], array[a] = array[a], array[b]
 
 def main():
     x = array.array('l', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -8,7 +6,7 @@ def main():
     swap(x, 2, 5)
     print(x)
 
-main()
+#main()
 
 #Merge Sort
 def mSplit(A):
@@ -21,7 +19,7 @@ def mSplit(A):
         else:
             o += i
         C = not(C) #condition inverter
-    return e, o
+    return [e, o]
 
 def mMerge(A):
     pass
@@ -48,3 +46,51 @@ def partition(A, low, high):
             i = i+1
     swap (A, i, high)
     return i
+def swap(array, a, b):
+    array[b], array[a] = array[a], array[b]
+    
+def mergeSort(A):
+    size = len(A)
+    temp = [0 for i in range(len(A))]
+    if size > 1:
+        middle = size // 2
+        leftA = A[:middle]
+        rightA = A[middle:]
+ 
+        leftA = mergeSort(leftA)
+        rightA = mergeSort(rightA)
+ 
+        p = 0
+        q = 0
+        r = 0
+ 
+        lenLeft = len(leftA)
+        lenRight = len(rightA)
+        while p < lenLeft and q < lenRight:
+            if leftA[p] < rightA[q]:
+              temp[r] = leftA[p]
+              p += 1
+            else:
+                temp[r] = rightA[q]
+                q += 1
+             
+            r += 1
+ 
+        
+        while p < lenLeft:
+            temp[r] = leftA[p]
+            p += 1
+            r += 1
+ 
+        while q < lenRight:
+            temp[r]=rightA[q]
+            q += 1
+            r += 1
+    else:
+        temp = list(A)
+    return temp
+
+testL = [5,3,4,2,1,6,8,7]
+testT = (5,3,4,8,7,1,2,6)
+print(mergeSort(testL))
+print(tuple(mergeSort(testT)))
